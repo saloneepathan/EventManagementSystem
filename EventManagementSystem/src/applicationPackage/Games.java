25ae53c26c9cf51da97c9c42aa23fa17c235c1fe
AditiNikam04 <naditi338@gmail.com>
2021-02-04 19:52:39 +0530

    Update Games.java

--- EventManagementSystem/src/applicationPackage/Games.java
+++ EventManagementSystem/src/applicationPackage/Games.java
@@ -1,5 +1,15 @@
 package applicationPackage;
 
 public class Games {
-
+	Games(String str){
+		if(str.equals("Silver")){
+		      System.out.println("You are offered 3 games in this package!!");
+		}
+		else if(str.equals("Gold")){
+		      System.out.println("You are offered 5 games in this package!!");
+		}
+		else if(str.equals("Platinum")){
+		      System.out.println("You are offered 7 games in this package!!");
+		}
+	}
 }
