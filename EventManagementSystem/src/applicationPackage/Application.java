3cecba46d1fafa0160cb5b2ec7622056a49fa5b9
AditiNikam04 <naditi338@gmail.com>
2021-02-05 15:29:56 +0530

    changes made

--- EventManagementSystem/src/applicationPackage/Application.java
+++ EventManagementSystem/src/applicationPackage/Application.java
@@ -11,23 +11,9 @@ public class Application
 		String event=sc.next();
 		System.out.println("Enter the package that you want to choose (Silver/Gold/Platinum) : ");
 		String packg=sc.next();
-		switch(packg) {
-			case "silver":
-				Meal m=new Meal(packg);
-				Games g=new Games(packg);
-				Host h=new Host();
-				break;
-			case "gold":
-				Meal m2=new Meal(packg);
-				Games g2=new Games(packg);
-				Host h2=new Host();
-				break;
-			case "platinum":
-				Meal m3=new Meal(packg);
-				Games g3=new Games(packg);
-				Host h3=new Host();
-				break;
-			
-		}
+		new Meal(packg);
+		new Games(packg);
+		new Host();
+		new Decoration(event,packg);
 	}
 }
--- EventManagementSystem/src/applicationPackage/Decoration.java
+++ EventManagementSystem/src/applicationPackage/Decoration.java
@@ -1,6 +1,20 @@
 package applicationPackage;
 
 public class Decoration {
+	
+	Decoration(String eventType, String packageType){
+		System.out.println("You have choosen the "+packageType+" package for your "+eventType+" event !!");
+		if(packageType.equalsIgnoreCase("Silver")) {
+			Silver(eventType);
+		}
+		else if(packageType.equalsIgnoreCase("Gold")) {
+			Gold(eventType);
+		}
+		else if(packageType.equalsIgnoreCase("Platinum")) {
+			Platinum(eventType);
+		}
+	}
+	
 	public static void Silver(String eventType) {
 		switch(eventType) {
 			case "corporate":
@@ -20,10 +34,45 @@ public class Decoration {
 				break;
 		}
 	}
+	
 	public static void Gold(String eventType) {
+		switch(eventType) {
+			case "corporate":
+				System.out.println("Chairs, with a satin cloth, to accomodate 500 people. 4 Flower bouquets, 2 tables, 1 dais");
+				break;
+			case "wedding":
+				System.out.println("Chairs, with a satin cloth, to accomodate 500 people, flower decorations(real flowers), 5 fancy lights, 2 personalized name cutout(250cm * 100cm)");
+				break;
+			case "social":
+				System.out.println("Decoration with 200 baloons, Chairs, with a satin cloth, to accomodate 200 people, 3 fancy lights, 2 personalized cutout (250cm * 100cm)");
+				break;
+			case "kids":
+				System.out.println("Decoration with 200 balloons depending upon your color theme. 3 Fancy lights, 3 cartoon cutout, 2 personalized name cutout(250cm * 100cm)");
+				break;
+			case "birthday":
+				System.out.println("Decoration with 200 balloons depending upon your color theme. 3 Fancy lights, 3 cartoon cutout, 2 personalized name cutout(250cm * 100cm)");
+				break;
+		}
 		
 	}
+	
 	public static void Platinum(String eventType) {
-		
+		switch(eventType) {
+			case "corporate":
+				System.out.println("Sofa chairs, to accomodate 800 people. Some flower decorations(false flowers), 4 Flower bouquets, 2 tables, 1 dais");
+				break;
+			case "wedding":
+				System.out.println("Chairs, with and ribbons, to accomodate 800 people, flower decorations(real flowers and leaves), 10 fancy lights, 2 personalized name cutout(250cm * 100cm), Flower Showers, Confetti Cannons");
+				break;
+			case "social":
+				System.out.println("Decoration with 300 baloons, Chairs, with a satin cloth and ribbons, to accomodate 500 people, 5 fancy lights, 2 personalized cutout (250cm * 100cm), 1 Large Disco Ball");
+				break;
+			case "kids":
+				System.out.println("Decoration with 300 balloons, and real flowers, depending upon your color theme. 5 Fancy lights, 3 cartoon cutout, 1 Human Cartoon, 2 personalized name cutout(250cm * 100cm)");
+				break;
+			case "birthday":
+				System.out.println("Decoration with 300 balloons, and real flowers, depending upon your color theme. 5 Fancy lights, 3 cartoon cutout, 1 Human Cartoon, 2 personalized name cutout(250cm * 100cm)");
+				break;
+		}
 	}
 }
