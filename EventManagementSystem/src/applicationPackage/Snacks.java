1174d028d61b1f1fa4161e4572b60f009edded6a
AditiNikam04 <naditi338@gmail.com>
2021-02-05 16:26:41 +0530

    2 files changed

--- EventManagementSystem/src/applicationPackage/Snacks.java
+++ EventManagementSystem/src/applicationPackage/Snacks.java
@@ -1,6 +1,82 @@
 package applicationPackage;
 
-public class Snacks 
-{
+public class Snacks{ 
 
+	Snacks(String eventType, String packageType){
+		if(packageType.equalsIgnoreCase("Silver"))
+		{
+			Silver(eventType);
+		}
+		else if(packageType.equalsIgnoreCase("Gold"))
+		{
+			Gold(eventType);
+		}
+		else if(packageType.equalsIgnoreCase("Platinum"))
+		{
+			Platinum(eventType);
+		}
+	}
+	
+	public static void Silver(String eventType) {
+		switch(eventType) 
+		{
+			case "corporate":
+				System.out.println("SNACKS : You will get samosas and cheese chilly toast for snacks.");
+				break;
+			case "wedding":
+				System.out.println("SNACKS : You will get samosas and cheese chilly toast for snacks.");
+				break;
+			case "social":
+				System.out.println("SNACKS : You will get burgers and cheese chilly toast for snacks.");
+				break;
+			case "kids":
+				System.out.println("SNACKS : You will get samosas and cheese chilly toast for snacks.");
+				break;
+			case "birthday":
+				System.out.println("SNACKS : You will get cheesy fries and pizza for snacks.");
+				break;
+		}
+	}
+	public static void Gold(String eventType) 
+	{
+		switch(eventType) 
+		{
+			case "corporate":
+				System.out.println("SNACKS : You will get samosas and cheese chilly toast for snacks.");
+				break;
+			case "wedding":
+				System.out.println("SNACKS : You will get cheesy fries and cheese chilly toast for snacks.");
+				break;
+			case "social":
+				System.out.println("SNACKS : You will get pizza and cheesy fries for snacks.");
+				break;
+			case "kids":
+				System.out.println("SNACKS : You will get samosas and cheese chilly toast for snacks.");
+				break;
+			case "birthday":
+				System.out.println("SNACKS : You will get burger and samosa for snacks.");
+				break;
+		}
+	}
+	public static void Platinum(String eventType) 
+	{
+		switch(eventType) 
+		{
+			case "corporate":
+				System.out.println("SNACKS : You will get wafers, a cupcake, samosas and cheese chilly toast for snacks.");
+				break;
+			case "wedding":
+				System.out.println("SNACKS : You will get burger, cheesy fries and cheese chilly toast for snacks.");
+				break;
+			case "social":
+				System.out.println("SNACKS : You will get burger, pizza and cheesy fries for snacks.");
+				break;
+			case "kids":
+				System.out.println("SNACKS : You will get wafers, burger, samosas and cheese chilly toast for snacks.");
+				break;
+			case "birthday":
+				System.out.println("SNACKS : You will get pizza, a cupcake, burger and samosa for snacks.");
+				break;
+		}
+	}
 }
--- EventManagementSystem/src/applicationPackage/SoftDrinks.java
+++ EventManagementSystem/src/applicationPackage/SoftDrinks.java
@@ -1,5 +1,82 @@
 package applicationPackage;
 
-public class SoftDrinks {
+public class SoftDrinks{ 
 
+	SoftDrinks(String eventType, String packageType){
+		if(packageType.equalsIgnoreCase("Silver"))
+		{
+			Silver(eventType);
+		}
+		else if(packageType.equalsIgnoreCase("Gold"))
+		{
+			Gold(eventType);
+		}
+		else if(packageType.equalsIgnoreCase("Platinum"))
+		{
+			Platinum(eventType);
+		}
+	}
+	
+	public static void Silver(String eventType) {
+		switch(eventType) 
+		{
+			case "corporate":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi for refreshment.");
+				break;
+			case "wedding":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi and Appy for refreshment.");
+				break;
+			case "social":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi for refreshment.");
+				break;
+			case "kids":
+				System.out.println("SOFT DRINKS : You will be offered with Maza and Appy for refreshment.");
+				break;
+			case "birthday":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi and Appy for refreshment.");
+				break;
+		}
+	}
+	public static void Gold(String eventType) 
+	{
+		switch(eventType) 
+		{
+			case "corporate":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi for refreshment.");
+				break;
+			case "wedding":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi and Appy for refreshment.");
+				break;
+			case "social":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi for refreshment.");
+				break;
+			case "kids":
+				System.out.println("SOFT DRINKS : You will be offered with Maza and Appy for refreshment.");
+				break;
+			case "birthday":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi and Appy for refreshment.");
+				break;
+		}
+	}
+	public static void Platinum(String eventType) 
+	{
+		switch(eventType) 
+		{
+			case "corporate":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi and Appy for refreshment.");
+				break;
+			case "wedding":
+				System.out.println("SOFT DRINKS : You will be offered with Sprite, Pepsi and Appy for refreshment.");
+				break;
+			case "social":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi for refreshment.");
+				break;
+			case "kids":
+				System.out.println("SOFT DRINKS : You will be offered with Maza and Appy for refreshment.");
+				break;
+			case "birthday":
+				System.out.println("SOFT DRINKS : You will be offered with Pepsi and Appy for refreshment.");
+				break;
+		}
+	}
 }
